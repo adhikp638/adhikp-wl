02abff4a640795823541636b3fa9382997eb4c05
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-01 23:33:16 +0000

    testing second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> HELLO KEDAAAAR....123</h1>
+        <h1> HELLO KEDAAAAR....123 and 234 </h1>
       </header>
     </div>
   );
