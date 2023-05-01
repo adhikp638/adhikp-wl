c8cdb89da7cb4c293f1832f54106631621207584
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-01 22:25:04 +0000

    testing from cloud9

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> HELLO KEDAAAAR....</h1>
+        <h1> HELLO KEDAAAAR....123</h1>
       </header>
     </div>
   );
