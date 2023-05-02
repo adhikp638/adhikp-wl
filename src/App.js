1155c6b5da63d83ad8ec41fe34ce26cdedb63a96
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-02 00:17:46 +0000

    testing third commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> HELLO KEDAAAAR....123 and 234 </h1>
+	  <button onClick='clicked()'/>
       </header>
     </div>
   );
