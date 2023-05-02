3a0d87f4028ec5ef59dd383dcb09ddd1f74f8a80
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-02 00:23:39 +0000

    testing 4th commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> HELLO KEDAAAAR....123 and 234 </h1>
-	  <button onClick='clicked()'/>
+	  <button size=20 onClick='clicked()'/>
       </header>
     </div>
   );
 }
-
+function clicked() {
+ this.document.h1.text="blah";
+}
 export default App;
