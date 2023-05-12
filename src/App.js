0240a7ef8068d79cf39a2997d70ed20d25b4d238
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-12 16:57:56 +0000

    trying font

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { slide as Menu } from 'react-burger-menu';
 function App() {
 	  return (
 		      <div>
-		        <Menu>
+		        <Menu styles={{ bmBurgerButton: { position: 'fixed', width: '36px', height: '30px', left: '36px', top: '36px' }, bmBurgerBars: { background: '#373a47' }, bmCrossButton: { height: '24px', width: '24px' }, bmCross: { background: '#bdc3c7' }, bmMenuWrap: { position: 'fixed', height: '100%' }, bmMenu: { background: '#373a47' }, bmMorphShape: { fill: '#373a47' }, bmItemList: { color: '#b8b7ad', padding: '0.8em' }, bmItem: { display: 'inline-block' }, bmOverlay: { background: 'rgba(0, 0, 0, 0.3)' } }}>>
 		          <a className="menu-item" href="/">
 		            Home
 		          </a>
