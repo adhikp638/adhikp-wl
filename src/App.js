e38b739d7bfb7d1631bf444899b063b94941cb17
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-12 21:41:05 +0000

    test at 5:41

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Tile } from "@kiwicom/orbit-components";
 
+import "./App.css";
+
 function App() {
 	  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
