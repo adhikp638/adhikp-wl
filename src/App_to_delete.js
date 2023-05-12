acb897bdca469b714e7e5714168f6e82d9c9d99e
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-12 15:36:40 +0000

    commiting at root level

---
+++ src/App_to_delete.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> HELLO KEDAAAAR....123 and 234 </h1>
+      </header>
+    </div>
+  );
+}
+export default App;
