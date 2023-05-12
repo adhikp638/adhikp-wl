a32bc77705fc6317d6a27da636d11d77471ddd4a
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-12 21:00:18 +0000

    comitting at 5:00PM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Tile from '@kiwicom/orbit-components/lib/Tile';
+import { Tile } from "@kiwicom/orbit-components";
 import "./Tile.css";
 
 function App() {
