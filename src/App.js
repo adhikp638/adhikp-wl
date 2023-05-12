834efb26d07f76a2df4e0dcca9e4dc6c7d7fb699
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-12 18:33:02 +0000

    still testing

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
-/* App.js */
+import React, { useState } from "react";
 import "./App.css";
 import Tile from "./Tile";
-import React, { useState } from "react";
 
 function App() {
-	  const [tiles, setTiles] = useState([
-		      { id: 1, text: "Tile 1" },
-		      { id: 2, text: "Tile 2" },
-		      { id: 3, text: "Tile 3" },
-		    ]);
+	  const [isOpen, setIsOpen] = useState(false);
+
+	  const toggleMenu = () => setIsOpen(!isOpen);
 
 	  return (
 		      <div className="App">
-		        <header>
+		        <header className="App-header">
+		          <div className="menu">
+		            <button className="burger-icon" onClick={toggleMenu}>
+		              <span className="line"></span>
+		              <span className="line"></span>
+		              <span className="line"></span>
+		            </button>
+		            {isOpen && (
+				                <div className="menu-items">
+				                  <a href="#">Menu Item 1</a>
+				                  <a href="#">Menu Item 2</a>
+				                  <a href="#">Menu Item 3</a>
+				                </div>
+				              )}
+		          </div>
 		          <div className="header-links">
-		            <a href="/">Link 1</a>
-		            <a href="/">Link 2</a>
-		            <a href="/">Link 3</a>
-		            <div className="burger-menu">
-		              <div className="menu">
-		                <a href="/">Link 1</a>
-		                <a href="/">Link 2</a>
-		                <a href="/">Link 3</a>
-		              </div>
-		              <svg
-		                xmlns="http://www.w3.org/2000/svg"
-		                width="32"
-		                height="32"
-		                viewBox="0 0 24 24"
-		                fill="#ffffff"
-		              >
-		                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-		              </svg>
-		            </div>
+		            <a href="#">Link 1</a>
+		            <a href="#">Link 2</a>
+		            <a href="#">Link 3</a>
 		          </div>
 		        </header>
-		        <div className="tile-container">
-		          {tiles.map((tile) => (
-				            <Tile key={tile.id} text={tile.text} />
-				          ))}
+		        <div className="tiles">
+		          <Tile title="Tile 1" />
+		          <Tile title="Tile 2" />
+		          <Tile title="Tile 3" />
 		        </div>
 		      </div>
 		    );
 }
 
 export default App;
+
