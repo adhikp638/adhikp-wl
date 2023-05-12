1f5a281789928985922aea9a0c165d2c9deb962e
EC2 Default User <ec2-user@ip-172-31-40-122.us-east-2.compute.internal>
2023-05-12 15:19:13 +0000

    Trying out burger menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> HELLO KEDAAAAR....123 and 234 </h1>
-      </header>
-    </div>
-  );
+	  return (
+		      <div>
+		        <Menu>
+		          <a className="menu-item" href="/">
+		            Home
+		          </a>
+		          <a className="menu-item" href="/about">
+		            About
+		          </a>
+		          <a className="menu-item" href="/contact">
+		            Contact
+		          </a>
+		        </Menu>
+		        <h1>Welcome to my React app!</h1>
+		        <p>This is my first React app with a burger menu.</p>
+		      </div>
+		    );
 }
+
 export default App;
