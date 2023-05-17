802a2f14e09fd31cd5b33fbfe6b42266423d7749
adhikp638 <adhikp638@gmail.com>
2023-05-16 20:32:00 -0400

    latest version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import logo from './wealthlane.svg';
+import eq_comp_graph from './eq_comp_graph.png';
 import BurgerMenu from "./BurgerMenu";
 import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import Flip from 'react-reveal/Flip';
+import Rotate from 'react-reveal/Rotate';
+
 import './App.css';
 
 function App() {
@@ -24,73 +29,429 @@ function App() {
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
 
     <div className="card-container">
       <Fade top distance="10%" duration={1500}>
-        <div className="card-image">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-        <div className="card-content">Reporting: Powerful reporting and analytics that helps you answer questions like</div>
-      </Fade>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
     </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
+    <div className="card-container">
+      <Fade top distance="10%" duration={1500}>
+        <div className="card-image"><img src={eq_comp_graph} className="body-image"/></div>
+       
+        <div className="card-content">
+              <p>
+			<ul>
+				<li><b><Zoom Right>$800 billion: </Zoom></b>The amount public companies will dish out as equity compensation over the next 10 years</li>
+                        <li><b><Zoom Right>$40 billion: </Zoom></b> The amount tech companies will dish out in just Restricted Stock Units</li>
+			</ul>			
+              </p>
+
+        </div>
+          
+    </Fade>
+    </div>
+
 
     <div className="footer">
 	<text>Some random content</text>
--- src/BurgerMenu.js
+++ src/BurgerMenu.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './BurgerMenu.css';
 
 const BurgerMenu = ({ isMenuOpen, handleMenuClick }) => {
-  const menuRef = React.useRef();
+  const menuRef = useRef();
 
   const handleClick = (e) => {
     if (menuRef.current && !menuRef.current.contains(e.target)) {
-      handleMenuClick();
+      handleMenuClick(false);
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     document.addEventListener('mousedown', handleClick);
 
     return () => {
@@ -19,14 +19,14 @@ const BurgerMenu = ({ isMenuOpen, handleMenuClick }) => {
   }, [handleMenuClick]);
 
   return (
-    <div className={`menu-container ${isMenuOpen ? 'open' : ''}`} onClick={handleMenuClick}>
-      <div className="burger-icon"></div>
-      <div className="burger-icon"></div>
-      <div className="burger-icon"></div>
+    <div className="container" onClick={() => handleMenuClick(!isMenuOpen)}>
+      <div className="burger-icon" />
+      <div className="burger-icon" />
+      <div className="burger-icon" />
 
       {isMenuOpen && (
         <div className="popup-container" ref={menuRef}>
-          <div className="menu-item">Menu Item 1</div>
+          <div className="menu-item"><a href="http://www.cnn.com">Menu Item 1</a></div>
           <div className="menu-item">Menu Item 2</div>
           <div className="menu-item">Menu Item 3</div>
         </div>
