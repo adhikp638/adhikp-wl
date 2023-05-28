522f7e6c8960888ef6115118d5f0206f250a723c
adhikp638 <adhikp638@gmail.com>
2023-05-27 22:19:47 -0400

    test

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import App_test from './App_test';
 import reportWebVitals from './reportWebVitals';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+
 
 ReactDOM.render(
   <React.StrictMode>
